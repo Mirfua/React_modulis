1607971a012a6005b9501564b66129104455e0fe
Jevgenijus <j.suchodolskis@gmail.com>
2021-10-21 13:35:58 +0300

    id generation & localStorage

--- my-app/src/Components/FieldAnimal.jsx
+++ my-app/src/Components/FieldAnimal.jsx
@@ -1,16 +1,28 @@
 
-function FieldAnimal({ fieldAnimal }) {
+function FieldAnimal({ fieldAnimal, field, goHome }) {
 
 
-    if ('cow' === fieldAnimal.animal) {
+    if (field === fieldAnimal.field && 'cow' === fieldAnimal.animal) {
         return (
             <div className="cow">
+                <h2>{fieldAnimal.id}</h2>
+                <button onClick={() => goHome(fieldAnimal.id)}>Go home</button>
             </div>
         );
     }
-    else if ('sheep' === fieldAnimal.animal) {
+    else if (field === fieldAnimal.field && 'sheep' === fieldAnimal.animal) {
         return (
             <div className="sheep">
+                <h2>{fieldAnimal.id}</h2>
+                <button onClick={() => goHome(fieldAnimal.id)}>Go home</button>
+            </div>
+        );
+    }
+    else if (field === fieldAnimal.field && 'horse' === fieldAnimal.animal) {
+        return (
+            <div className="horse">
+                <h2>{fieldAnimal.id}</h2>
+                <button onClick={() => goHome(fieldAnimal.id)}>Go home</button>
             </div>
         );
     }
